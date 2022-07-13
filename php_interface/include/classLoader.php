d0ff8bf045c4666f1d4196adb4085af0f3329acd
Yanovich Alexandr <yanovicham@gmail.com>
2022-07-13 23:12:07 +0300

    new

--- php_interface/include/classLoader.php
+++ php_interface/include/classLoader.php
@@ -7,6 +7,7 @@ Bitrix\Main\Loader::registerAutoLoadClasses(null, [
     'Extra\ProductPrice' => '/local/php_interface/classes/Extra/ProductPrice.php',
     'Extra\BitrixUser' => '/local/php_interface/classes/Extra/BitrixUser.php',
     'Custom\CrmCompany' => '/local/php_interface/classes/Custom/CrmCompany.php',
+    'Custom\QuantitativeAccounting' => '/local/php_interface/classes/Custom/QuantitativeAccounting.php',
 
 ]);
 
